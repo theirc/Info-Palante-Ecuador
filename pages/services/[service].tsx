257535c02b546a253459895b004658a3740d0d3d
Bianca Chami <b.chami39@gmail.com>
2024-02-27 11:44:26 +0000

    feat: updated footer strings

--- pages/services/[service].tsx
+++ pages/services/[service].tsx
@@ -131,6 +131,7 @@ export default function Service({
             currentLocale={locale}
             locales={LOCALES}
             strings={strings.footerStrings}
+            additionalInfo="Si necesitas información segura y estás en otro país:"
             links={footerLinks}
             signpostVersion={publicRuntimeConfig?.version}
           />
